27900057039bdd2607f9067156c41928f30fdb6a
michalpu <michalpurtak1@gmail.com>
2018-04-30 18:05:16 +0200

    first commit

---
+++ src/main/java/com/michalpu/booksapihomework/BooksApiHomeworkApplication.java
@@ -0,0 +1,12 @@
+package com.michalpu.booksapihomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksApiHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApiHomeworkApplication.class, args);
+	}
+}
